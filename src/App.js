b2d2b20c4f5970de0b650011982030268a41c5c9
Peter Lin <szul@uw.edu>
2020-02-03 17:35:49 +0800

    [feature] react routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Button from "./components/Button";
+import { BrowserRouter } from "react-router-dom";
+import ROUTES, { RenderRoutes } from "./helpers/routes";
 
 function App() {
   return (
-    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
-      <h1>Super cool page</h1>
-      <Button onClick={() => console.log("I was clicked")}>
-        I am a button
-      </Button>
-    </div>
+    <BrowserRouter>
+      <RenderRoutes routes={ROUTES} />
+    </BrowserRouter>
   );
 }
 
