e497d378b297d59ed83b1abe377bef1a6ae3a29c
Peter Lin <szul@uw.edu>
2020-02-04 18:15:28 +0800

    [modify] add eslint with airbnb

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import ROUTES, { RenderRoutes } from "./helpers/routes";
-import { useSelector } from "react-redux";
-import { IntlProvider } from "react-intl";
-import getLocaleData from "./locales";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { IntlProvider } from 'react-intl';
+import ROUTES, { RenderRoutes } from './helpers/routes';
+import getLocaleData from './locales';
 
 function App() {
   const { language } = useSelector(state => state.user);
