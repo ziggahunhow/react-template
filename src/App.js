75d43d0288cffed60d5f6a01e6b31352d9980f60
ziggahunhow <szul@uw.edu>
2020-02-17 15:23:58 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { IntlProvider } from 'react-intl';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import { useSelector } from 'react-redux';
 import ROUTES, { RenderRoutes } from './helpers/routes';
 import getLocaleData from './locales';
 
 function App() {
   const { language } = useSelector(state => state.user);
+
   return (
     <IntlProvider locale={language} messages={getLocaleData(language)}>
       <BrowserRouter>
-        <RenderRoutes routes={ROUTES} />
+        <Container maxWidth="lg" className="pt-10">
+          <CssBaseline />
+          <RenderRoutes routes={ROUTES} />
+        </Container>
       </BrowserRouter>
     </IntlProvider>
   );
